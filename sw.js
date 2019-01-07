055d5b3e7b5aa47e01ab58b45bdbacdaddaf8274
lily <742020663@qq.com>
2019-01-07 11:27:00 -0600

    Modified manifest.json

--- sw.js
+++ sw.js
@@ -34,9 +34,19 @@ self.addEventListener('install',(event) =>{
               'js/dbhelper.js',
               'js/main.js',
               'js/restaurant_info.js',
-            'restaurant.html',
             'sw.js',
-            'index.html']
+            'index.html',
+            'restaurant.html?id=1',
+            'restaurant.html?id=2',
+            'restaurant.html?id=3',
+            'restaurant.html?id=4',
+            'restaurant.html?id=5',
+            'restaurant.html?id=6',
+            'restaurant.html?id=7',
+            'restaurant.html?id=8',
+            'restaurant.html?id=9',
+            'restaurant.html?id=10',
+        ]
         );
         })
     );
