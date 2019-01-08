79e91bf6884ec5ab5902e5a20fe33bae10f8536a
lily <742020663@qq.com>
2019-01-07 19:24:27 -0600

    Modified manifest.json

--- sw.js
+++ sw.js
@@ -37,26 +37,31 @@ self.addEventListener('install',(event) =>{
               'js/restaurant_info.js',
             'sw.js',
             'index.html',
-            'restaurant.html?id=1',
-            'restaurant.html?id=2',
-            'restaurant.html?id=3',
-            'restaurant.html?id=4',
-            'restaurant.html?id=5',
-            'restaurant.html?id=6',
-            'restaurant.html?id=7',
-            'restaurant.html?id=8',
-            'restaurant.html?id=9',
-            'restaurant.html?id=10'
+            'restaurant.html'
         ]
         );
         })
     );
 })
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then(function(response){
-            return response || fetch(event.request)
-        })
-    );
-    });
+////reference to https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook/#on-network-response
+    self.addEventListener('fetch', function(event) {
+        event.respondWith(
+          caches.open(staticCache).then(function(cache) {
+            return cache.match(event.request).then(function (response) {
+              return response || fetch(event.request).then(function(response) {
+                cache.put(event.request, response.clone());
+                return response;
+              });
+            });
+          })
+        );
+      });
+
+// self.addEventListener('fetch', (event) => {
+//     event.respondWith(
+//         caches.match(event.request).then(function(response){
+//             return response || fetch(event.request)
+//         })
+//     );
+//     });
